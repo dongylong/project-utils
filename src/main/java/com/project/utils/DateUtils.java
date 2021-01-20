fbf7741285a073332e4d3655871b7bdf1a2ea79d
dongyl <dongyuanlong@tal.com>
2021-01-20 23:02:03 +0800

    add DateUtils

--- src/main/java/com/project/utils/DateUtils.java
+++ src/main/java/com/project/utils/DateUtils.java
@@ -1,5 +1,29 @@
 package com.project.utils;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
 public class DateUtils {
 
+    /**
+     *
+     * @param localDate
+     * @return
+     */
+    public static LocalDateTime dayStart(LocalDate localDate) {
+        LocalDateTime time = LocalDateTime.of(localDate, LocalTime.MIN);
+        return time;
+    }
+
+    /**
+     *
+     * @param localDate
+     * @return
+     */
+    public static LocalDateTime dayEnd(LocalDate localDate) {
+        LocalDateTime time = LocalDateTime.of(localDate, LocalTime.MAX);
+        return time;
+    }
 }
