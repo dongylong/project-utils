90e0f69e47c5cac8faa292615acf2b52ef38d294
dongyl <dongyuanlong@tal.com>
2021-01-20 23:13:53 +0800

    add ThreadUtils

--- src/main/java/com/project/utils/CollectionUtils.java
+++ src/main/java/com/project/utils/CollectionUtils.java
@@ -7,15 +7,12 @@ import java.util.stream.Collectors;
 public class CollectionUtils {
 
     public static Map<Integer, List<ObjectDemo>> list2KeyListMap(List<ObjectDemo> demoList) {
-
         Map<Integer, List<ObjectDemo>> objectDemoMap = demoList.stream()
                 .collect(Collectors.groupingBy(ObjectDemo::getNum));
         return objectDemoMap;
-
     }
 
     public static Map<Integer, ObjectDemo> list2Map(List<ObjectDemo> demoList) {
-
         Map<Integer, ObjectDemo> objectDemoMap = demoList.stream().
                 collect(Collectors
                         .toMap(ObjectDemo::getNum, a -> a, (v1, v2) -> v2));
@@ -23,15 +20,14 @@ public class CollectionUtils {
     }
 
     public static String map2String(Map<String, ObjectDemo> objectDemoMap) {
-
         String demoNames = objectDemoMap.values().stream()
                 .map(ObjectDemo::getStr)
                 .collect(Collectors.joining(","));
         return demoNames;
     }
+
     public static Long listCount(List<ObjectDemo> demoList) {
         Long count = demoList.stream().distinct().count();
         return count;
     }
-
 }
---
+++ src/main/java/com/project/utils/ThreadUtils.java
@@ -0,0 +1,19 @@
+package com.project.utils;
+
+import org.slf4j.MDC;
+
+import java.util.Map;
+
+public class ThreadUtils {
+
+    public static void copyMDCProperty(){
+        Map<String, String> mdcCopy = MDC.getCopyOfContextMap();
+        try{
+            MDC.setContextMap(mdcCopy);
+        }catch (Exception e){
+
+        }finally {
+            MDC.clear();
+        }
+    }
+}
