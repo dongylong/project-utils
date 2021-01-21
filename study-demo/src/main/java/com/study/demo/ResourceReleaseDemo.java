7cd84bb33c29779c998f5c312b8dbd360f74f3a6
dongyl <dongyuanlong@tal.com>
2021-01-21 23:41:15 +0800

    add ResourceReleaseDemo

---
+++ study-demo/src/main/java/com/study/demo/ResourceReleaseDemo.java
@@ -0,0 +1,32 @@
+package com.study.demo;
+
+import java.io.*;
+
+public class ResourceReleaseDemo implements AutoCloseable{
+    @Override
+    public void close() throws Exception {
+        System.out.println("resource release");
+    }
+    public void use() throws Exception {
+        try (ResourceReleaseDemo resourceRelease = new ResourceReleaseDemo()){
+            System.out.println("use resource");
+        }
+    }
+
+    /**
+     * 多资源管理
+     * @param fromPath
+     * @param toPath
+     * @throws IOException
+     */
+    public void copyFile(String fromPath,String toPath) throws IOException{
+        try (InputStream input = new FileInputStream(fromPath);
+             OutputStream output = new FileOutputStream(toPath)){
+            byte[] buffer = new byte[8192];
+            int len = -1;
+            while ((len = input.read(buffer))!= -1){
+                output.write(buffer,0,len);
+            }
+        }
+    }
+}
