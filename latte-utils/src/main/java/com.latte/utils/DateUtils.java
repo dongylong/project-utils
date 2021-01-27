8a52ddb01dc97bd957611e2dfb5290502b75c310
dongyl <dongyuanlong@100tal.com>
2021-01-27 16:36:20 +0800

    add DateUtils

--- latte-utils/src/main/java/com.latte/utils/DateUtils.java
+++ latte-utils/src/main/java/com.latte/utils/DateUtils.java
@@ -3,6 +3,7 @@ package com.latte.utils;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.TemporalAdjusters;
 import java.util.List;
 
 public class DateUtils {
@@ -25,5 +26,13 @@ public class DateUtils {
     public static LocalDateTime dayEnd(LocalDate localDate) {
         LocalDateTime time = LocalDateTime.of(localDate, LocalTime.MAX);
         return time;
+    }
+    public static LocalDate queryLastDay(LocalDate localDate) {
+        LocalDate lastDay = localDate.with(TemporalAdjusters.lastDayOfMonth());
+        return lastDay;
+    }
+    public static LocalDate queryFirstDay(LocalDate localDate) {
+        LocalDate firstDay = LocalDate.of(localDate.getYear(), localDate.getMonthValue(), 1);
+        return firstDay;
     }
 }
