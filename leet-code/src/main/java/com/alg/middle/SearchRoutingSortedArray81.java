68fc36e9e062d582fc91d4abe7ab55566419142d
dongyl <dongyuanlong@live.cn>
2021-04-07 23:23:29 +0800

    add leet-code

---
+++ leet-code/src/main/java/com/alg/middle/SearchRoutingSortedArray81.java
@@ -0,0 +1,40 @@
+package com.alg.middle;
+
+public class SearchRoutingSortedArray81 {
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return false;
+        }
+        if (n == 1) {
+            return nums[0] == target;
+        }
+        int l = 0;
+        int r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[l] == nums[mid]
+                    && nums[mid] == nums[r]) {
+                ++l;
+                --r;
+            } else if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target
+                        && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
