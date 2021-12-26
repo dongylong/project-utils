c5971ec3090537f063d48eff6a7eec4ce3317399
dongyl <dongyuanlong@tal.com>
2021-12-26 19:36:23 +0800

    add leet 318

---
+++ leet-code/src/main/java/com/alg/middle/MaxProduct318.java
@@ -0,0 +1,9 @@
+package com.alg.middle;
+
+/**
+ * 给定一个字符串数组 words，找到 length(word[i]) * length(word[j]) 的最大值，
+ * 并且这两个单词不含有公共字母。你可以认为每个单词只包含小写字母。如果不存在这样的两个单词，返回 0。
+ */
+public class MaxProduct318 {
+
+}
