d65127697cabf0a659ba6ef50d754e04bb10f1ae
yuanlong.dong <yuanlong.dong@niutron.com>
2022-11-28 21:24:38 +0800

    add

---
+++ leet-code/src/main/java/com/alg/middle/MagicNum878.java
@@ -0,0 +1,28 @@
+package com.alg.middle;
+
+public class MagicNum878 {
+    private static final int MOD= 1000000007;
+    public int nthMagicalNumber(int n ,int a , int b){
+        long l = Math.min(a,b);
+        long r = (long) n*Math.min(a,b);
+        int c = lcm(a,b);
+        while (l<=r){
+            long mid = (l+r)/2;
+            long cnt = mid /a +mid/b -mid /c;
+            if (cnt>=n){
+                r= mid -1;
+            }else{
+                l = mid +1;
+            }
+        }
+
+        return (int) ((r+1)%MOD);
+    }
+    public int lcm(int a ,int b){
+        return a*b/gcd(a,b);
+    }
+
+    private int gcd(int a, int b) {
+        return b !=0?gcd(b,a%b):a;
+    }
+}
