ee3453d1c6f0c744657b6f3a10d7772277122855
dongyl <328224304@qq.com>
2023-01-29 16:19:37 +0800

    add enc

---
+++ encrypt-decrypt/src/main/java/com/demo/bytebit/ByteBitDemo.java
@@ -0,0 +1,25 @@
+package com.demo.bytebit;
+
+/**
+ * ByteBitDemo
+ *
+ * @Description:
+ */
+public class ByteBitDemo {
+    /**
+     * 根据编码的格式不一样，对应的字节也不一样
+     * 如果是UTF-8:一个中文对应的是三个字节
+     * 如果是GBK : 一个中文对应的是二个字节
+     * <p>
+     * 如果是英文，就无所谓编码格式
+     */
+    public static void main(String[] args) throws Exception {
+        String a = "A";
+        byte[] bytes = a.getBytes();
+        for (byte aByte : bytes) {
+            System.out.println(aByte);
+            String s = Integer.toBinaryString(aByte);
+            System.out.println(s);
+        }
+    }
+}
